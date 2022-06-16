7499d942fa9f9da22750cfb85cae053072994344
Branislav Madzar <branislav.madzar.bvi@gmail.com>
2022-06-17 00:19:30 +0200

    UserCommon Class and required enums

---
+++ WebShopREST/src/beans/UserCommon.java
@@ -0,0 +1,18 @@
+package beans;
+
+import java.util.Date;
+
+import beans.CustomerType;
+import enums.Role;
+
+public class UserCommon {
+	private String userName;
+	private String password;
+	private String name;
+	private String surname;
+	private String sex;
+	private Date birthDate;
+	private Role role;
+	private CustomerType customerType;
+
+}
---
+++ WebShopREST/src/interfaces/UserInterface.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+import java.util.Date;
+
+import beans.CustomerType;
+
+public interface UserInterface {
+
+}
