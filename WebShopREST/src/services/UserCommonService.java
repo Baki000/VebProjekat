4a4184d00c0a8369d4d9a1aa0690457ff86b01fe
Branislav Madzar <branislav.madzar.bvi@gmail.com>
2022-06-17 16:51:32 +0200

    UserCommonService changed

--- WebShopREST/src/services/UserCommonService.java
+++ WebShopREST/src/services/UserCommonService.java
@@ -5,17 +5,22 @@ import java.util.Collection;
 import javax.annotation.PostConstruct;
 import javax.servlet.ServletContext;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-import beans.Letovanje;
 import beans.Product;
+import beans.User;
 import beans.UserCommon;
-import dao.LetovanjeDAO;
 import dao.ProductDAO;
+import dao.UserCommonDAO;
+import dao.UserDAO;
 
 @Path("/userCommon")
 public class UserCommonService {
@@ -47,5 +52,36 @@ public class UserCommonService {
 	public Collection<UserCommon> getAllUsers(){
 		System.out.println("DOBIO SAM ZAHTEV");
 		return getUserCommonDAO().getAllUsers();
+	}
+	
+	@POST
+	@Path("/login")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response login(UserCommon user, @Context HttpServletRequest request) {
+		UserCommonDAO userDao = (UserCommonDAO) ctx.getAttribute("userCommonDAO");
+		UserCommon loggedUser = userDao.find(user.getUserName(), user.getPassword());
+		if (loggedUser != null) {
+			return Response.status(400).entity("Invalid username and/or password").build();
+		}
+		request.getSession().setAttribute("user", loggedUser);
+		return Response.status(200).build();
+	}
+	
+	
+	@POST
+	@Path("/logout")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public void logout(@Context HttpServletRequest request) {
+		request.getSession().invalidate();
+	}
+	
+	@GET
+	@Path("/currentUser")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public UserCommon login(@Context HttpServletRequest request) {
+		return (UserCommon) request.getSession().getAttribute("user");
 	}
 }
